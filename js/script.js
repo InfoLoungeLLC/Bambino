b2c58a4ae8ef79cd11c2b47907141e2188521f1a
YUUYA <yuuya081@icloud.com>
2020-06-08 16:50:34 +0900

    Markup TopPage

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+function hamburger() {
+    document.getElementById('line1').classList.toggle('header-sec__target-inner__a');
+    document.getElementById('line2').classList.toggle('header-sec__target-inner__b');
+    document.getElementById('line3').classList.toggle('header-sec__target-inner__c');
+    document.getElementById('target').classList.toggle('slidex');
+    document.getElementById('nav_f').classList.toggle('header-sec__fadein');
+}
+
+document.getElementById('target').addEventListener('click', function() {
+    hamburger();
+})
+
+let list = document.getElementsByClassName('list');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function() {
+        hamburger(); 
+    })
+}
+*/
+
+/*
+new Vue({
+    el:'#app',
+    data: {
+        activ:false,
+        navi: false,
+    },
+    methods: {
+        naviOpen: function() {
+            this.active = !this.navi;
+            this.navi = !this.navi;
+        }
+    }
+})
+*/
